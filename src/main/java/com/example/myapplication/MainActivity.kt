c1e8a0d66a841f59ad4b1ddd074d42244d88a79b
lili <joudalabsi14@gmail.com>
2026-05-23 10:21:02 +0300

    add joud vpn files

---
+++ src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.myapplication
+import android.content.Intent
+import android.net.VpnService
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+class MainActivity : ComponentActivity() {
+
+    private val vpnPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            startService(Intent(this, LinkGuardVpnService::class.java))
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Button(onClick = { startVpn() }) {
+                        Text("Start LinkGuard VPN")
+                    }
+                }
+            }
+        }
+    }
+
+    private fun startVpn() {
+        val intent = VpnService.prepare(this)
+        if (intent != null) {
+            vpnPermissionLauncher.launch(intent)
+        } else {
+            startService(Intent(this, LinkGuardVpnService::class.java))
+            Toast.makeText(this, "LinkGuard VPN Started!", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
